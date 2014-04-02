ff7564306eb130f5738dc3fa65a11a419dced7cb
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-02 13:12:30 +0100

    Working basic task creation.

---
+++ strands_executive_msgs/src/strands_executive_msgs/task_utils.py
@@ -0,0 +1,10 @@
+import rospy
+from ros_datacentre_msgs.msg import StringPair
+from strands_executive_msgs.msg import Task
+
+def add_string_argument(task, string_arg):
+	task.arguments.append(StringPair(second=string_arg))
+
+
+def add_object_id_argument(task, oid, msg_type):
+	task.arguments.append(StringPair(first=msg_type._type, second=oid))
