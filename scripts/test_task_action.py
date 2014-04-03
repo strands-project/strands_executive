8ef76a76877cb5c898457d8a5e52de4946b9f2f5
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-03 10:51:37 +0100

    Moved test action to task_executor, adding server to provide it.

---
+++ scripts/test_task_action.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from strands_executive_msgs.msg import Task
+from task_executor.msg import *
+
+
+class TestTaskAction(object):
+    def __init__(self):
+        rospy.init_node("test_task_action")
+        self.server = actionlib.SimpleActionServer('test_task', TestExecutionAction, self.execute, False)
+        self.server.start() 
+
+    def execute(self, goal):
+        # Do lots of awesome groundbreaking robot stuff here
+        self.server.set_succeeded()
+
+
+if __name__ == '__main__':
+    executor = TestTaskAction()    
+    rospy.spin()
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['task_executor'],
-    scripts=['scripts/example_task_client.py'],
-    package_dir={'': 'src'}
+    # packages=['task_executor'],
+    scripts=['scripts/example_task_client.py', 'scripts/task_executor_node.py', 'scripts/test_task_action.py'],
+    # package_dir={'': 'src'}
 )
 
 setup(**d)
