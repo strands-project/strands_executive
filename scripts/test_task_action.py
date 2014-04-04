1fce96f5277185b982651f58f930d1394325b7a1
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-04 14:27:47 +0100

    Navigation added for faked action server.

--- scripts/test_task_action.py
+++ scripts/test_task_action.py
@@ -4,19 +4,28 @@ import rospy
 import actionlib
 from strands_executive_msgs.msg import Task
 from task_executor.msg import *
-
+from topological_navigation.msg import GotoNodeAction
 
 class TestTaskAction(object):
     def __init__(self):
         rospy.init_node("test_task_action")
-        self.server = actionlib.SimpleActionServer('test_task', TestExecutionAction, self.execute, False)
-        self.server.start() 
+        self.task_server = actionlib.SimpleActionServer('test_task', TestExecutionAction, self.execute, False)
+        self.task_server.start() 
+        self.nav_server = actionlib.SimpleActionServer('topological_navigation', GotoNodeAction, execute_cb = self.nav_callback, auto_start = False)
+        self.nav_server.start() 
+
 
     def execute(self, goal):
         print 'called with goal %s'%goal
-        rospy.sleep(5)
+        rospy.sleep(1)
         print 'done here'
-        self.server.set_succeeded()
+        self.task_server.set_succeeded()
+
+    def nav_callback(self, goal):
+        print 'called with nav goal %s'%goal
+        rospy.sleep(1)
+        print 'done nav'
+        self.nav_server.set_succeeded()
 
 
 if __name__ == '__main__':
