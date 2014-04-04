b715446c971c94292ee555d0656b550487a1966c
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-04 10:39:51 +0100

    Abstracted basic functionality into base class

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    # packages=['task_executor'],
-    scripts=['scripts/example_task_client.py', 'scripts/task_executor_node.py', 'scripts/test_task_action.py'],
-    # package_dir={'': 'src'}
+    packages=['task_executor'],
+    scripts=['scripts/example_task_client.py', 'scripts/fifo_task_executor.py', 'scripts/test_task_action.py'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
