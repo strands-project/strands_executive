c038b2724e0833f88f6678abf645b677f2a43ae6
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-13 20:30:26 +0100

    Expanding schedule executor.

--- task_executor/scripts/scheduled_task_executor.py
+++ task_executor/scripts/scheduled_task_executor.py
@@ -4,21 +4,33 @@ import rospy
 from Queue import Queue, Empty
 from strands_executive_msgs.msg import Task
 from task_executor.base_executor import AbstractTaskExecutor
-
+from threading import Thread
+from task_executor.execution_schedule import ExecutionSchedule
 
 class ScheduledTaskExecutor(AbstractTaskExecutor):
 
     def __init__(self):
         # init node first, must be done before call to super init for service advertising to work
-        rospy.init_node("task_executor")
+        rospy.init_node("task_executor", log_level=rospy.DEBUG)
 
         # init superclasses
         super( ScheduledTaskExecutor, self ).__init__()
 
         # defaults for setting the ends of tasks
         self.default_duration = rospy.Duration.from_sec(60 * 60 * 4)
-        # self.default_execution_cutoff = 
+        
+        # storage for tasks which have been added but not considered 
+        self.unscheduled_tasks = Queue()
        
+        # data structure that manages tasks
+        self.execution_schedule = ExecutionSchedule()
+
+        self.scheduling_thread = Thread(target=self.schedule_tasks)
+        self.scheduling_thread.start()
+
+        self.execution_thread = Thread(target=self.execute_tasks)
+        self.execution_thread.start()
+
     def get_default_end_time(self, start_time):
         return start_time + self.default_duration
 
@@ -35,12 +47,45 @@ class ScheduledTaskExecutor(AbstractTaskExecutor):
         """ Called with a new task for the executor """
         self.fill_times(task)
         print task
+        self.unscheduled_tasks.put(task)
 
-    def run_executor(self):
+    def call_scheduler(self, tasks):
+        
+
+    def schedule_tasks(self):
+        loopSecs = 5
+        
+        while not rospy.is_shutdown():           
+            print "scheduling thread %s" % rospy.is_shutdown()      
+            try:
+                unscheduled = []
+                # block until at least one task is available
+                unscheduled.append(self.unscheduled_tasks.get(True, loopSecs))
+                # now check for any remaining tasks in the queue
+                try:
+                    while True:
+                        unscheduled.append(self.unscheduled_tasks.get(False))
+                except Empty, e:
+                    pass
+                
+                rospy.logdebug('Got a further %s tasks to schedule' % len(unscheduled))
+
+                self.execution_schedule.add_new_tasks(unscheduled)
+
+                (order, start_times) = self.call_scheduler(self.execution_schedule.get_schedulable_tasks())
+
+
+            except Empty, e:
+                rospy.logdebug('No new tasks to schedule')
+
+
+
+    def execute_tasks(self):
         r = rospy.Rate(1) # 1hz
         
         while not rospy.is_shutdown():           
 
+            print "executing thread %s" % rospy.is_shutdown()
             # dequeue tasks to schedule
 
             # if current schedule empty, create schedule and execute
@@ -54,6 +99,14 @@ class ScheduledTaskExecutor(AbstractTaskExecutor):
             r.sleep()
         
 
+    # def wait_for_exit(self):
+    #     self.scheduling_thread.join()
+    #     self.execution_thread.join()
+
 if __name__ == '__main__':
-    executor = ScheduledTaskExecutor()    
-    executor.run_executor()    
+    executor = ScheduledTaskExecutor()        
+    rospy.spin()
+
+
+# create a schedule class which handles blocking until execution and manages the various changes
+
--- task_executor/src/task_executor/execution_schedule.py
+++ task_executor/src/task_executor/execution_schedule.py
@@ -1,18 +1,41 @@
+from strands_executive_msgs.msg import Task
+from threading import Condition
+
 
 class ExecutionSchedule(object):
-	""" Class for holding and managing tasks for execution. Allows changes to be made to the tasks to be executed and blocking until execution time."""
+
     def __init__(self):
+    	self.current_task = None
+    	self.exection_change = Condition()
+    	self.tasks = []
+
+    def add_new_tasks(self, tasks):
+    	""" Add new tasks to be scheduled. """
+    	self.tasks.extend(tasks)
+
+    def get_schedulable_tasks(self):
+    	""" Get the tasks which are available to be scheduled. Does not include the task being executed. """
+    	return self.tasks
+
+    def set_schedule(self, order, start_times):
     	pass
 
+	# # wait for next task
+	# # blocks until its time to start execution of the next task
+	# # this will be either the next task in the schedule or when task execution needs to be alter due to a change
+	# # returns and enum indicating which of these it its
+
+	# def get_current_task(self):
+	# 	""" Get the next task for execution. """
+	# 	return self.current_task
 
 
-
-	# wait for next task
-	# blocks until its time to start execution of the next task
-	# this will be either the next task in the schedule or when task execution needs to be alter due to a change
-	# returns and enum indicating which of these it its
-
-	# get current task - the head of the execution queue
-
-	# execution complete
-	# option to reduce gaps in schedule without calling scheduler
+	# # execution complete
+	# # option to reduce gaps in schedule without calling scheduler
+	
+	# def wait_for_execution_change(self):
+	# 	""" Blocks until there is a change in execution status """
+	# 	self.exection_change.acquire()
+	# 	self.exection_change.wait()
+	# 	self.exection_change.release()
+	# 	return ExecutionStatus.NEXT_TASK
