25994b3e5af159477619043cbffe738812e0a935
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-15 10:01:10 +0100

    Actions executed from schedule, but only first.
    Fixed bug in base_executor where missing wait_for_server() caused silent blocking.

--- src/task_executor/execution_schedule.py
+++ src/task_executor/execution_schedule.py
@@ -1,16 +1,26 @@
 from strands_executive_msgs.msg import Task
-from threading import Condition
+from threading import Event
 from copy import deepcopy
 from Queue import Queue, Empty
 from collections import deque
+
+
+# def enum(*sequential, **named):
+#     """ 
+#     Pre-v3 emum support from http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+#     """
+#     enums = dict(zip(sequential, range(len(sequential))), **named)
+#     return type('Enum', (), enums)
+
+# ExecutionStatus = enum('ZERO', 'ONE', 'TWO')
 
 class ExecutionSchedule(object):
 
     def __init__(self):
     	self.current_task = None
-    	self.exection_change = Condition()
-    	self.exection_queue = deque()
-    	self.tasks = []
+    	self.execution_change = Event()
+    	self.execution_queue = deque()
+    	self.tasks = []        
 
     def add_new_tasks(self, tasks):
     	""" Add new tasks to be scheduled. """
@@ -19,6 +29,11 @@ class ExecutionSchedule(object):
     def get_schedulable_tasks(self):
     	""" Get the tasks which are available to be scheduled. Does not include the task being executed. """
     	return deepcopy(self.tasks)
+
+    def execute_next_task(self):
+        self.current_task = self.execution_queue.popleft()
+        self.execution_change.set()
+
 
     def set_schedule(self, scheduled_tasks):
     	"""
@@ -33,32 +48,38 @@ class ExecutionSchedule(object):
     		return False
 
     	for scheduled in scheduled_tasks:
-    		if not any(t.task_id == scheduled.task for t in self.tasks):
+    		if not any(t.task_id == scheduled.task_id for t in self.tasks):
     			rospy.loginfo('Trying to scheduled a missed task')
     			return False
 
     	# now clear out the execution queue so that the new schedule comes into effect after current execution completes
-    	self.exection_queue.clear()
-    	self.exection_queue.extend(scheduled_tasks)
-    	if self.current_task == None:
-    		# start execution
-
-	# # wait for next task
-	# # blocks until its time to start execution of the next task
-	# # this will be either the next task in the schedule or when task execution needs to be alter due to a change
-	# # returns and enum indicating which of these it its
-
-	# def get_current_task(self):
-	# 	""" Get the next task for execution. """
-	# 	return self.current_task
+    	self.execution_queue.clear()
+    	self.execution_queue.extend(scheduled_tasks)
+    	
+        # if nothing is executing, make sure something starts
+        if self.current_task == None:
+            self.execute_next_task()            
 
 
-	# # execution complete
-	# # option to reduce gaps in schedule without calling scheduler
-	
-	# def wait_for_execution_change(self):
-	# 	""" Blocks until there is a change in execution status """
-	# 	self.exection_change.acquire()
-	# 	self.exection_change.wait()
-	# 	self.exection_change.release()
-	# 	return ExecutionStatus.NEXT_TASK
+
+    def get_current_task(self):
+        """ Get the next task for execution. """
+        return self.current_task
+
+
+    def wait_for_execution_change(self, timeout):
+        """ 
+        Blocks until current_task changes value 
+        """
+        self.execution_change.wait(timeout)
+
+        # if change was notified
+        if self.execution_change.is_set():
+            # reset to cause future calls to block until next notification
+            self.execution_change.clear()
+            return True
+        else:
+            # timeout
+            return False
+
+
