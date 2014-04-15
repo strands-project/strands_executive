44cf620d9d6b0a985f38f89eaa0d5c4d84054cc1
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-15 12:49:14 +0100

    Compilation under linux now.

--- scipoptsuite/create-lib-links.bash
+++ scipoptsuite/create-lib-links.bash
@@ -7,3 +7,9 @@ for lib in lib*.a ; do
   echo ln -sf $lib $target.a
   ln -sf $lib $target.a
 done
+
+for lib in lib*gnu.opt.so ; do
+  target=`echo $lib | cut -d '-' -f 1`
+  echo ln -sf $lib $target.so
+  ln -sf $lib $target.so
+done
