c27776484995e152c329c83913f5a4a8dff78b43
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-28 17:03:54 +0100

    Added wait node action with service to preempt whatever wait is in progress.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['wait_action'],
-    scripts=['scripts/wait_node.py'],
+    scripts=['scripts/wait_node.py', 'scripts/wait_node_client.py'],
 )
 
 setup(**d)
