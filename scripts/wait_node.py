4d55049386d3a8757ea94fdc28d05df8b19f5c50
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-04-29 18:01:26 +0100

    Fixed preemption.

--- scripts/wait_node.py
+++ scripts/wait_node.py
@@ -7,64 +7,64 @@ from datetime import *
 from std_srvs.srv import Empty
 
 class WaitServer:
-  def __init__(self): 
-  	self.end_wait_srv = rospy.Service('/wait_action/end_wait', Empty, self.end_wait) 
-  	self.server = actionlib.SimpleActionServer('wait_action', WaitAction, self.execute, False) 
-  	self.server.start()
+    def __init__(self): 
+        self.end_wait_srv = rospy.Service('/wait_action/end_wait', Empty, self.end_wait) 
+        self.server = actionlib.SimpleActionServer('wait_action', WaitAction, self.execute, False) 
+        self.server.start()
 
 
 
-  def end_wait(self, req):
-  	rospy.loginfo("Preempting sleep")
-  	if self.server.is_active():
-  		self.server.set_preempted()
+    def end_wait(self, req):
+        rospy.loginfo("Preempting sleep")
+        if self.server.is_active():
+            self.server.set_preempted()
 
-  def execute(self, goal):
-  	# rospy.loginfo("waiting: %s" % goal) 
+    def execute(self, goal):
+        # rospy.loginfo("waiting: %s" % goal) 
 
-  	now = rospy.get_rostime()
-  	target = goal.wait_until
-  	if target.secs == 0:
-  		target = now + goal.wait_duration
+        now = rospy.get_rostime()
+        target = goal.wait_until
+        if target.secs == 0:
+            target = now + goal.wait_duration
 
-  	if target == now:
-  		# 72 hours
-  		a_really_long_time = rospy.Duration(60 * 60 * 24 * 3)
-  		target = now + a_really_long_time
-  		# rospy.loginfo("waiting a really long time")
+        if target == now:
+            # 72 hours
+            a_really_long_time = rospy.Duration(60 * 60 * 24 * 3)
+            target = now + a_really_long_time
+            # rospy.loginfo("waiting a really long time")
 
 
-	rospy.loginfo("target wait time: %s" % datetime.fromtimestamp(target.secs))	
+        rospy.loginfo("target wait time: %s" % datetime.fromtimestamp(target.secs))	
 
-	# how often to provide feedback 
-	feedback_secs = 5
-	feedback = WaitFeedback()
-	# how long to wait
-	wait_duration_secs = target.secs - now.secs
+        # how often to provide feedback 
+        feedback_secs = 5
+        feedback = WaitFeedback()
+        # how long to wait
+        wait_duration_secs = target.secs - now.secs
 
-	# the rate to publish feedback at/check time at
-	r = rospy.Rate(1.0/feedback_secs)
-	
-	count = 1.0
-	feedback_steps = wait_duration_secs/feedback_secs
-	while not rospy.is_shutdown() and self.server.is_active() and rospy.get_rostime() < target:
-		feedback.percent_complete = count/feedback_steps
-		self.server.publish_feedback(feedback)
-		count += 1
-		r.sleep()
+        # the rate to publish feedback at/check time at
+        r = rospy.Rate(1.0/feedback_secs)
 
-	rospy.loginfo("waited until: %s" % datetime.fromtimestamp(rospy.get_rostime().secs))	
+        count = 1.0
+        feedback_steps = wait_duration_secs/feedback_secs
+        while not rospy.is_shutdown() and not self.server.is_preempt_requested() and rospy.get_rostime() < target:
+            feedback.percent_complete = count/feedback_steps
+            self.server.publish_feedback(feedback)
+            count += 1
+            r.sleep()
 
-	if self.server.is_active():
-  		self.server.set_succeeded()
+        rospy.loginfo("waited until: %s" % datetime.fromtimestamp(rospy.get_rostime().secs))
 
+        if self.server.is_preempt_requested():
+            self.server.set_preempted()
+        else:
+            self.server.set_succeeded()
 
 
 if __name__ == '__main__':
-
     rospy.init_node("wait_node")
-    
-    # wait for simulated time to kick in as rospy.get_rostime() is 0 until first clock message received
+
+# wait for simulated time to kick in as rospy.get_rostime() is 0 until first clock message received
     while not rospy.is_shutdown() and rospy.get_rostime().secs == 0:
         pass
 
