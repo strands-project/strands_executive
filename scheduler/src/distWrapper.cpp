ba43dce74ced1f461ad4b7a899172b4b51f03c1c
Bruno Lacerda <bruno@bob.(none)>
2014-05-06 19:16:28 +0100

    getting scheduler to call right expected time service

--- scheduler/src/distWrapper.cpp
+++ scheduler/src/distWrapper.cpp
@@ -9,7 +9,7 @@ double DistWrapper::dist(string p1, string p2)
 {
     
     ros::NodeHandle n;
-    ros::ServiceClient expectedTimeClient = n.serviceClient<strands_executive_msgs::GetExpectedTravelTime>("/mdp_plan_exec/get_expected_travel_time");
+    ros::ServiceClient expectedTimeClient = n.serviceClient<strands_executive_msgs::GetExpectedTravelTime>("/mdp_plan_exec/get_expected_travel_time_to_node");
     strands_executive_msgs::GetExpectedTravelTime srv;
     srv.request.start_id= p1;
     srv.request.target_id=p2;
