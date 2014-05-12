54d1d0f1b6a0f5efe2e56199e007f4aebf3cac6e
Bruno Lacerda <bruno@bruno-dell.(none)>
2014-05-12 15:23:35 +0100

    reads mdp with states labels initial and target at the same time

--- mdp_plan_exec/src/mdp_plan_exec/mdp.py
+++ mdp_plan_exec/src/mdp_plan_exec/mdp.py
@@ -278,35 +278,7 @@ class ProductMdp(Mdp):
             prop_line.append(False)
             self.prop_map[i]=prop_line
         
-    
-    #def read_rewards(self,original_mdp):
-        
-        #f = open(original_mdp, 'r')
-         
-        #for line in f:
-            ##print(line)
-            ##print('rewards "time"\n')
-            #if line.startswith('rewards'):
-                #break
-                
-        #self.rewards=[[0]*self.n_actions for i in range(self.n_states)]
-        
-        #for line in f:
-            #if line.startswith('endrewards'):
-                #break
-            #line=line.split()
-            #if line !=[]:
-                #action=self.actions.index(line[0].strip('[]'))
-                #rest=line[1].split('=')[1].split(':')
-                #state=int(rest[0])
-                #reward=float(rest[1].rstrip(';'))
-                #for i in range(0,self.n_states):
-                    #if i not in self.goal_states:
-                        #if self.state_labels[i][1]==state:
-                            #if self.transitions[i][action]:
-                                #self.rewards[i][action]=reward  
-        #f.close()
-        
+
        
         
         
@@ -389,11 +361,16 @@ class ProductMdp(Mdp):
         
         self.goal_states=[]
         for line in f:
-            int_line=[int(line.split(':')[0]),int(line.split(':')[1])]
-            if int_line[1]==target_index:
-                self.goal_states.append(int_line[0])
-            if int_line[1]==init_index:
-                self.initial_state=int_line[0]
+            line=line.split(':')
+            state_index=int(line[0])
+            labels=line[1].split(' ')
+            del labels[0]
+            n_labels=len(labels)
+            for i in range(0,n_labels):
+                if int(labels[i])==target_index:
+                    self.goal_states.append(state_index)
+                if int(labels[i])==init_index:
+                    self.initial_state=state_index
         
         f.close()
         
@@ -430,128 +407,4 @@ class ProductMdp(Mdp):
             if self.prop_map[next_possible_state][final_node_prop_index]:
                 return next_possible_state
         return -1
-    
-#if __name__ == '__main__':
-    
-    #graph=sys.argv[1]
-    #mdp=sys.argv[2]
-
-    
-    
-    #a=NavMDP(graph)
-    
-    #a.write_prism_model(mdp)
-    
-    
-    
-#class NavMDP(MDP):
-    #def __init__(self,nav_graph):
-        #self.read_graph(nav_graph)
-        
-        
-    #def read_graph(self,nav_graph):
-        #f = open(nav_graph, 'r')
-        
-        #self.initial_state=0
-        
-        
-        #line=f.readline()
-        #line=line.split(' ')
-        #self.n_nodes=int(line[0])
-        #self.n_edges=int(line[1])
-        #self.n_states=self.n_nodes+2*self.n_edges+1
-        
-        #self.n_props=self.n_nodes+2
-        #self.props=[None]*self.n_props
-        
-        #for i in range(0,self.n_props-2):
-            #self.props[i]='v' + str(i)
-            
-        #self.props[self.n_props-2]='failure'
-        #self.props[self.n_props-1]='fatal_failure'
-        
-        
-        #self.prop_map=[[False]*self.n_props for i in range(self.n_states)]
-        
-        #for i in range(0,self.n_props-2):
-            #self.prop_map[i][i]=True
-            
-        #for i in range(self.n_nodes,self.n_nodes+self.n_edges):
-            #self.prop_map[i][self.n_props-2]=True
-            
-        #self.prop_map[self.n_states-1][self.n_props-1]=True
-        
-        
-        
-        #self.n_actions=self.n_edges+1
-        #self.actions=[None]*self.n_actions
-        #self.actions[self.n_actions-1]='recover'
-        
-        #self.transitions=[[False]*self.n_actions for i in range(self.n_states)]
-        
-        
-        #self.rewards=[[0]*self.n_actions for i in range(self.n_states)]
-        
-        
-        #current_line=0
-        #for line in f:
-            #line=line.split()
-            #from_node=int(line[0])
-            #to_node=int(line[1])
-            #suc_rate=float(line[2])
-            #self.actions[current_line]='goto'+str(from_node) + 'p' + str(to_node)
-            #self.transitions[from_node][current_line]=[[to_node,suc_rate]]
-            #if suc_rate<1:
-                #self.transitions[from_node][current_line].append([self.n_nodes+current_line,round(1-suc_rate,3)])
-                #i=0
-                #finished_rec_states=False
-                #while not finished_rec_states:
-                    #if not line[3+i+1]=='+':
-                        #finished_rec_states=True
-                    #current_trans=line[3+i].split(':')
-                    #prob=float(current_trans[0])
-                    #next_node=int(current_trans[1])
-                    #if i==0:
-                        #if from_node==next_node:
-                            #self.transitions[self.n_nodes+current_line][self.n_edges]=[[self.n_nodes+self.n_edges+current_line,prob]]
-                        #else:
-                            #self.transitions[self.n_nodes+current_line][self.n_edges]=[[next_node,prob]]
-                    #else:
-                        #if from_node==next_node:
-                            #self.transitions[self.n_nodes+current_line][self.n_edges].append([self.n_nodes+self.n_edges+current_line,prob])
-                        #else:   
-                            #self.transitions[self.n_nodes+current_line][self.n_edges].append([next_node,prob])
-                    #i+=2
-                #self.rewards[self.n_nodes+current_line][self.n_actions-1]=max(round(float(line[3+i])-float(line[3+i-1]),3),0)  
-                #self.rewards[from_node][current_line]=float(line[3+i-1])
-            #else:
-                #self.rewards[from_node][current_line]=float(line[3])
-                            
-    
-                    
-                    
-            #current_line+=1
-            
-        
-        #for i in range(0,self.n_edges):
-            #current_action=self.actions[i]
-            #nodes=current_action.split('goto')[1].split('p')
-            #from_node=int(nodes[0])
-            #to_node=int(nodes[1])
-            #if not self.transitions[from_node][i][0][1]==1:
-                #self.transitions[from_node+self.n_nodes+self.n_edges][:]=self.transitions[from_node][:]
-                #self.transitions[from_node+self.n_nodes+self.n_edges][i]=False
-                #self.rewards[from_node+self.n_nodes+self.n_edges][:]=self.rewards[from_node][:]
-                #self.rewards[from_node+self.n_nodes+self.n_edges][i]=0
-                #self.prop_map[from_node+self.n_nodes+self.n_edges][from_node]=True 
-        
-                
-
-        ##print self.rewards        
-        ##print self.prop_map
-        #print self.transitions
-        
-        
-        
-        #f.close()    
     
