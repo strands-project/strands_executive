ec6f5fff862538201ef104000a088fe5a92b1405
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-23 08:52:00 +0200

    Changes for local testing.

--- task_executor/scripts/test_task_action.py
+++ task_executor/scripts/test_task_action.py
@@ -6,5 +6,6 @@ from task_executor.utils import TestTaskAction
 if __name__ == '__main__':
     rospy.init_node('test_task_action')
     actual_action_duration = rospy.Duration(600)    
-    action_server = TestTaskAction(expected_action_duration=actual_action_duration)
+    action_server = TestTaskAction(expected_action_duration=actual_action_duration, expected_drive_duration=rospy.Duration(3))
+    action_server.start()
     rospy.spin()
