6880359b408f69b860ea3898757e3d32e629b46b
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-23 11:33:04 +0200

    Updated test executor to match mdp expectations, so now uses monitored navigation.

--- task_executor/src/task_executor/utils.py
+++ task_executor/src/task_executor/utils.py
@@ -1,10 +1,11 @@
 import rospy
-import actionlib
+import actionlib 
 from std_msgs.msg import String
 from strands_executive_msgs.msg import Task
 from task_executor.msg import *
 from topological_navigation.msg import GotoNodeAction, GotoNodeResult
-
+from strands_navigation_msgs.msg import NavStatistics, MonitoredNavigationAction, MonitoredNavigationResult, MonitoredNavigationGoal
+from actionlib_msgs.msg import GoalStatus
 
 class TestTaskAction(object):
     """ 
@@ -13,8 +14,11 @@ class TestTaskAction(object):
     def __init__(self, expected_action_duration=rospy.Duration(1), expected_drive_duration=rospy.Duration(20)):
         self.expected_action_duration = expected_action_duration
         self.expected_drive_duration = expected_drive_duration
-        self.result   =  GotoNodeResult()    
+        self.nav_result   =  GotoNodeResult()    
+        self.mon_nav_result =  MonitoredNavigationResult()    
         self.nav_server = actionlib.SimpleActionServer('topological_navigation', GotoNodeAction, execute_cb = self.nav_callback, auto_start = False)
+        self.mon_nav_server = actionlib.SimpleActionServer('monitored_navigation', MonitoredNavigationAction, execute_cb = self.mon_nav_callback, auto_start = False)
+
         # self.task_server = actionlib.SimpleActionServer('test_task', TestExecutionAction, execute_cb = self.execute, auto_start = False)        
         self.cn_pub = rospy.Publisher('/current_node', String)
         self.cl_pub = rospy.Publisher('/closest_node', String)
@@ -23,6 +27,7 @@ class TestTaskAction(object):
         
     def start(self):
         self.nav_server.start()         
+        self.mon_nav_server.start()         
         # self.task_server.start() 
         while not rospy.is_shutdown():
              self.cn_pub.publish(String(self.cn))
@@ -42,23 +47,58 @@ class TestTaskAction(object):
         else:
             print "done normal"            
             self.task_server.set_succeeded()
+
+    def mon_nav_callback(self, goal):
+        print 'called with mon_nav goal'
+        target = rospy.get_rostime() + self.expected_drive_duration
+
+        # take some time to get there
+        while not rospy.is_shutdown() and rospy.get_rostime() < target and not self.mon_nav_server.is_preempt_requested():
+            rospy.sleep(0.1)           
+
+        if self.mon_nav_server.is_preempt_requested():
+            print "done mon_nav preempted"
+            self.mon_nav_result.sm_outcome = MonitoredNavigationResult.PREEMPTED
+            self.mon_nav_server.set_preempted(self.mon_nav_result)
+        else:
+            print "done mon_nav normal"                         
+            self.mon_nav_result.sm_outcome = MonitoredNavigationResult.SUCCEEDED       
+            self.mon_nav_server.set_succeeded(self.mon_nav_result)
+
+        print "mon_nav complete" 
         
 
     def nav_callback(self, goal):
         print 'called with nav goal %s'%goal.target
-        target = rospy.get_rostime() + self.expected_drive_duration
 
-        if goal.target != self.cn:
-            while not rospy.is_shutdown() and rospy.get_rostime() < target and not self.nav_server.is_preempt_requested():
-                rospy.sleep(0.1)           
+        self.mon_nav_action_client = actionlib.SimpleActionClient('monitored_navigation', MonitoredNavigationAction)
+        rospy.loginfo('waiting for mon nav server')
+        self.mon_nav_action_client.wait_for_server()
+        rospy.loginfo('done')
+
+        mon_goal = MonitoredNavigationGoal()
+        self.mon_nav_action_client.send_goal(mon_goal)
+        self.mon_nav_action_client.wait_for_result(rospy.Duration(5))
+
+        # wait for completion or prempt
+        while not rospy.is_shutdown() and self.mon_nav_action_client.get_state() == GoalStatus.ACTIVE and not self.nav_server.is_preempt_requested():
+            self.mon_nav_action_client.wait_for_result(rospy.Duration(0.1))
 
         if self.nav_server.is_preempt_requested():
             print "done preempted"
-            self.result.success = False
-            self.nav_server.set_preempted(self.result)
+            self.mon_nav_action_client.cancel_all_goals()            
+            self.nav_result.success = False
+            self.nav_server.set_preempted(self.nav_result)
+        elif self.mon_nav_action_client.get_state() != GoalStatus.SUCCEEDED:
+            print "done mon not SUCCEEDED"            
+            self.nav_result.success = False
+            self.nav_server.set_aborted(self.nav_result)
         else:
             print "done normal"     
             self.cn = goal.target            
-            self.result.success = True       
-            self.nav_server.set_succeeded(self.result)
+            self.nav_result.success = True       
+            self.nav_server.set_succeeded(self.nav_result)
+        
+
+
         print "nav complete" 
