27f6aaf7b4e0b17056275f4c70c3f4a5e9175d8e
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-24 11:02:58 +0100

    Working preempts on action too.
    Seems clean and robust for now.

--- scripts/test_task_action.py
+++ scripts/test_task_action.py
@@ -5,8 +5,8 @@ from task_executor.utils import TestTaskAction
 
 if __name__ == '__main__':
     rospy.init_node('test_task_action')
-    action_duration = rospy.Duration(60)    
-    drive_duration = rospy.Duration(60)    
+    action_duration = rospy.Duration(6)    
+    drive_duration = rospy.Duration(6)    
     action_server = TestTaskAction(expected_action_duration=action_duration, expected_drive_duration=drive_duration)
     action_server.start()
     rospy.spin()
