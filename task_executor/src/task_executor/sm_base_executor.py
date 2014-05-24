22c57821a2b7f24ef2c05538d9ed05cfb7920838
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-24 17:07:23 +0100

    Logging working from state machine now.

--- task_executor/src/task_executor/sm_base_executor.py
+++ task_executor/src/task_executor/sm_base_executor.py
@@ -13,7 +13,7 @@ from strands_executive_msgs.msg import ExecutePolicyAction, ExecutePolicyGoal
 from std_srvs.srv import Empty, EmptyResponse
 from std_msgs.msg import String
 from task_executor.base_executor import BaseTaskExecutor
-from smach_ros import SimpleActionState
+from smach_ros import SimpleActionState, IntrospectionServer
 import smach
 import threading
 
@@ -51,7 +51,7 @@ class TaskFailed(ExecutorState):
         rospy.logwarn('Task failed')
         completed = userdata.task
         self.executor.active_task = None
-        
+        self.executor.log_task_event(userdata.task, TaskEvent.TASK_FAILED, rospy.get_rostime())        
         self.executor.task_failed(completed)
         rospy.loginfo('Execution of task %s failed' % userdata.task.task_id)
         return 'aborted'
@@ -65,7 +65,8 @@ class TaskSucceeded(ExecutorState):
         # do bookkeeping before causing update
         completed = userdata.task
         self.executor.active_task = None
-        
+        self.executor.log_task_event(userdata.task, TaskEvent.TASK_SUCCEEDED, rospy.get_rostime())
+
         self.executor.task_succeeded(completed)
         rospy.loginfo('Execution of task %s succeeded' % userdata.task.task_id)
         return 'succeeded'              
@@ -77,7 +78,7 @@ class TaskCancelled(ExecutorState):
 
     def execute(self, userdata):
         rospy.loginfo('Execution of task %s was cancelled' % userdata.task.task_id)
-
+        self.executor.log_task_event(userdata.task, TaskEvent.TASK_PREEMPTED, rospy.get_rostime())
         # it could be that a delayed cancellation signal causes this to get called out of turn, so check that this is the correct cancellation
         if self.executor.active_task is not None and self.executor.active_task.task_id == userdata.task.task_id:
             completed = userdata.task
@@ -92,6 +93,7 @@ class TaskInitialisation(ExecutorState):
 
 
     def execute(self, userdata):
+        self.executor.log_task_event(userdata.task, TaskEvent.TASK_STARTED, rospy.get_rostime())
         self.executor.prepare_task(userdata.task)
         return 'succeeded'
 
@@ -102,14 +104,61 @@ class AbstractTaskExecutor(BaseTaskExecutor):
 
     def __init__(self):
         super( AbstractTaskExecutor, self ).__init__()
-        self.reset_sm()
-
-    def reset_sm(self):
         self.task_sm = None
         self.smach_thread = None
+        self.sis = None
+
+
+    def reset_sm(self):
+
+        # If introspection is running, stop it
+        if self.sis is not None:
+            self.sis.stop()
+
+        # If the smach_tread is running then we may have started executing prior to the sm winding up fully, or there is something blocking there        
+        timeout_secs = 15
+        if not self.join_smach_thread(timeout_secs):
+            rospy.logerr('SMACH thread has not shut down properly. Continuing with execution regardless')
+
+
+        self.task_sm = None
+        self.smach_thread = None
+        self.sis = None
+
+    def nav_start_cb(self, userdata, initial_states):
+        task = userdata.task
+        rospy.loginfo('Nav started to %s for task %s, action %s' % (task.start_node_id, task.task_id, task.action))
+        self.log_task_event(task, TaskEvent.NAVIGATION_STARTED, rospy.get_rostime())                        
+
+    def nav_termination_cb(self, userdata, terminal_states, container_outcome):
+        rospy.loginfo('Nav terminated with outcome %s' % container_outcome)
+        if container_outcome == 'succeeded':
+            self.log_task_event(userdata.task, TaskEvent.NAVIGATION_SUCCEEDED, rospy.get_rostime())                        
+        elif container_outcome == 'preempted':
+            self.log_task_event(userdata.task, TaskEvent.NAVIGATION_PREEMPTED, rospy.get_rostime())                        
+        else:
+            self.log_task_event(userdata.task, TaskEvent.NAVIGATION_FAILED, rospy.get_rostime())                        
+
+    def action_start_cb(self, userdata, initial_states):
+        task = userdata.task
+        rospy.loginfo('Action %s started for task %s' % (task.action, task.task_id))
+        self.log_task_event(task, TaskEvent.EXECUTION_STARTED, rospy.get_rostime())                        
+
+
+    def action_termination_cb(self, userdata, terminal_states, container_outcome):
+        rospy.loginfo('Action terminated with outcome %s' % container_outcome)
+        if container_outcome == 'succeeded':
+            self.log_task_event(userdata.task, TaskEvent.EXECUTION_SUCCEEDED, rospy.get_rostime())                        
+        elif container_outcome == 'preempted':
+            self.log_task_event(userdata.task, TaskEvent.EXECUTION_PREEMPTED, rospy.get_rostime())                        
+        else:
+            self.log_task_event(userdata.task, TaskEvent.EXECUTION_FAILED, rospy.get_rostime())                        
 
     def execute_task(self, task):
         """ Called to trigger execution of given task. """
+
+        # clean up from last execution
+        self.reset_sm()
 
         rospy.loginfo('Execution of task %s was requested' % task.task_id)
 
@@ -148,6 +197,7 @@ class AbstractTaskExecutor(BaseTaskExecutor):
 
             # when one child terminates, preempt the others
             concurrence_child_term_cb = lambda so: True
+            
 
             # navigation action
             if task.start_node_id != '':
@@ -167,6 +217,10 @@ class AbstractTaskExecutor(BaseTaskExecutor):
                                         default_outcome='succeeded',
                                         outcome_map=concurrence_outcome_map,
                                         child_termination_cb=concurrence_child_term_cb)
+                # register callback for logging
+                nav_concurrence.register_start_cb(self.nav_start_cb)
+                nav_concurrence.register_termination_cb(self.nav_termination_cb)
+                nav_concurrence.userdata.task = task
 
                 with nav_concurrence:
                     # where we want to go
@@ -197,6 +251,11 @@ class AbstractTaskExecutor(BaseTaskExecutor):
                                         outcome_map=concurrence_outcome_map,
                                         child_termination_cb=concurrence_child_term_cb)
 
+                # register callback for logging
+                action_concurrence.register_start_cb(self.action_start_cb)
+                action_concurrence.register_termination_cb(self.action_termination_cb)
+                action_concurrence.userdata.task = task
+
                 with action_concurrence:
                     smach.Concurrence.add('MONITORED', SimpleActionState(task.action, action_clz, goal=goal))
                     wiggle_room = rospy.Duration(5)
@@ -208,8 +267,21 @@ class AbstractTaskExecutor(BaseTaskExecutor):
                                 
         self.task_sm.set_initial_state(['TASK_INITIALISATION'])
 
+        self.sis = IntrospectionServer('task_executor', self.task_sm, '/TASK_ROOT')
+        self.sis.start()
+
         self.smach_thread = threading.Thread(target=self.task_sm.execute)
         self.smach_thread.start()
+
+    def join_smach_thread(self, timeout):
+        """ Returns true of the smach_thread has been terminated successfully """
+        # if we need to join
+        if self.smach_thread is not None and self.smach_thread.is_alive():
+            self.smach_thread.join(timeout)
+            return not self.smach_thread.is_alive()
+        else:
+            # else thread is not alive any more
+            return True
 
     def cancel_active_task(self):
         preempt_timeout_secs = 30
@@ -217,8 +289,8 @@ class AbstractTaskExecutor(BaseTaskExecutor):
             rospy.loginfo('Requesting preempt on state machine in state %s' % self.task_sm.get_active_states())
             self.task_sm.request_preempt()
             rospy.loginfo('Waiting for exit')
-            self.smach_thread.join(preempt_timeout_secs)
-            if self.smach_thread.is_alive():
+            
+            if not self.join_smach_thread(preempt_timeout_secs):
                 rospy.logerr('Task action or navigation did not preempt after %s seconds. State at end was %s' % (preempt_timeout_secs, self.task_sm.get_active_states()))
                 # manually notify completeness in this case
                 completed = self.active_task
