b461533a51bd1381fd0dab084fd995233eac59c4
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-11-12 20:13:28 +0000

    Corrected setup.py

--- sim_clock/setup.py
+++ sim_clock/setup.py
@@ -3,8 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['sim_clock'],
-    package_dir={'': 'src'}
+
 )
 
 setup(**d)
