0376c6441b306a8f5ba8cd006ab6a24b5cc42fd9
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-11-12 19:10:21 +0000

    Updated install targets.
    Conflicts:
    	task_executor/CMakeLists.txt

--- wait_action/setup.py
+++ wait_action/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['wait_action'],
-    scripts=['scripts/wait_node.py', 'scripts/wait_node_client.py'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
