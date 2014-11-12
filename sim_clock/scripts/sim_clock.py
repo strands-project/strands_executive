c707842a1576a05862fbe65b216fa71fc07eb28f
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-11-12 10:37:25 +0000

    Moved params to private namespace.

--- sim_clock/scripts/sim_clock.py
+++ sim_clock/scripts/sim_clock.py
@@ -15,14 +15,14 @@ if __name__ == '__main__':
     # make sure everyone else started after no uses sim time
     rospy.set_param('use_sim_time', True)
 
-    clock_pub = rospy.Publisher('/clock', Clock)
+    clock_pub = rospy.Publisher('/clock', Clock, queue_size=1)
 
     # how much to increment the simulated clock per second, default 1 minute per second
-    seconds_per_second = rospy.get_param('sim_time_step', 60)
+    seconds_per_second = rospy.get_param('~sim_time_step', 60)
     rospy.set_param('sim_time_step', seconds_per_second)
 
     # how often to update clock
-    updates_per_second = rospy.get_param('sim_time_update_freq', 10.0)
+    updates_per_second = rospy.get_param('~sim_time_update_freq', 10.0)
     rospy.get_param('sim_time_update_freq', updates_per_second)
     
     hz = rospy.Rate(updates_per_second)
