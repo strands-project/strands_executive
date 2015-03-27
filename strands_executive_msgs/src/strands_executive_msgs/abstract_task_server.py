cc13fc5794a3874a20c92fe5816f7513314bc912
Marc Hanheide <marc@hanheide.de>
2015-03-27 12:07:35 +0000

    using yaml to instantiate tasks

--- strands_executive_msgs/src/strands_executive_msgs/abstract_task_server.py
+++ strands_executive_msgs/src/strands_executive_msgs/abstract_task_server.py
@@ -3,31 +3,59 @@
 import rospy
 import actionlib
 from strands_executive_msgs.srv import IsTaskInterruptible, CreateTask
+from strands_executive_msgs.msg import Task
 from actionlib.msg import TestAction
+from mongodb_store_msgs.msg import StringPair
+
+
 
 class AbstractTaskServer(object):
-    def __init__(self, name, action_type=TestAction, interruptible=True):         
+    def __init__(self, name, action_type=TestAction, interruptible=True):
         self.name = name
         self.action_type = action_type
         self.interruptible = interruptible
-        self.server = actionlib.SimpleActionServer(self.name, self.action_type, self.execute, False) 
+        self.server = actionlib.SimpleActionServer(self.name,
+                                                   self.action_type,
+                                                   self.execute, False)
         self.server.start()
-        rospy.Service('%s_is_interruptible' % self.name, IsTaskInterruptible, self.is_interruptible)
+        rospy.Service('%s_is_interruptible' % self.name,
+                      IsTaskInterruptible, self.is_interruptible)
         rospy.Service('%s_create' % self.name, CreateTask, self.create)
 
     def is_interruptible(self, req):
         return self.interuptible
 
+    def _fill_slots(self, src, dest):
+        for s in dest.__slots__:
+            if s in src:
+                if s == 'arguments':
+                    # special case for attributes
+                    for a in src[s]:
+                        sp = StringPair()
+                        self._fill_slots(a, sp)
+                        dest.arguments.append(sp)
+                else:
+                    # a bit of a hacky way to check if we need to recurse...
+                    if str(type(dest.__getattribute__(s))).\
+                        startswith('<class ') \
+                            or type(dest.__getattribute__(s)) == dict:
+                        self._fill_slots(src[s], dest.__getattribute__(s))
+                    elif type(dest.__getattribute__(s)) == list:
+                        dest.__getattribute__(s).extend(src[s])
+                    else:
+                        dest.__setattr__(s, src[s])
+
     def create(self, req):
-        task = req.task
+        task = Task()
         task.max_duration = (task.end_before - task.start_after) / 2
         task.action = self.name
         if task.end_node_id is None:
             task.end_node_id = task.start_node_id
+        self._fill_slots(task, req.yaml)
         return task
 
     def execute(self, goal):
-        rospy.loginfo("dummy task goal is: %s" % goal) 
+        rospy.loginfo("dummy task goal is: %s" % goal)
         if self.server.is_preempt_requested():
             self.server.set_preempted()
         else:
@@ -36,7 +64,6 @@ class AbstractTaskServer(object):
 if __name__ == '__main__':
     rospy.init_node("abstract_task_server")
 
-# wait for simulated time to kick in as rospy.get_rostime() is 0 until first clock message received
     while not rospy.is_shutdown() and rospy.get_rostime().secs == 0:
         pass
 
