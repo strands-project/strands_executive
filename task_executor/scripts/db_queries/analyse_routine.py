212cdece010da9cbbd791c4ecbecf477a9cd2b12
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-05-19 15:30:48 +0200

    Adding more analysis tools

--- task_executor/scripts/db_queries/analyse_routine.py
+++ task_executor/scripts/db_queries/analyse_routine.py
@@ -16,20 +16,32 @@ import cmd
 
 class RoutineAnalyser(cmd.Cmd):
 
-    def __init__(self, routine_pairs):
+    def __init__(self, msg_store, routine_pairs):
         cmd.Cmd.__init__(self)
         # super(RoutineAnalyser, self).__init__()
         self.routine_pairs = routine_pairs
+        self.msg_store = msg_store
+
+    def check_idx(self, idx):
+        if idx < 0 or idx >= len(self.routine_pairs):
+            print 'idx must be greater than 0 and less than %s' % len(self.routine_pairs)
+            return False
+        else:
+            return True
+
 
     def do_merge(self, idx):    
         try:
-            idx = int(idx)
-            if idx >= 0 and idx < len(self.routine_pairs) - 1:
-                print 'merging %s into %s' % (idx, idx+1)
-                self.routine_pairs[idx+1] = (self.routine_pairs[idx][0], self.routine_pairs[idx+1][1])
-                del self.routine_pairs[idx]
+            if idx == 'all':
+                self.routine_pairs = [(self.routine_pairs[0][0], self.routine_pairs[-1][1])]
             else:
-                print 'invalid routine index, valid range from 0 to %s' % (len(self.routine_pairs) - 2)
+                idx = int(idx)
+                if idx >= 0 and idx < len(self.routine_pairs) - 1:
+                    print 'merging %s into %s' % (idx, idx+1)
+                    self.routine_pairs[idx+1] = (self.routine_pairs[idx][0], self.routine_pairs[idx+1][1])
+                    del self.routine_pairs[idx]
+                else:
+                    print 'invalid routine index, valid range from 0 to %s' % (len(self.routine_pairs) - 2)
         except ValueError, e:
             print 'provided argument was not an int: %s' % idx
 
@@ -45,13 +57,76 @@ class RoutineAnalyser(cmd.Cmd):
                         )
 
             print 'routine %s: %s to %s, duration: %s, tasks: %s' % (i, start, end, end-start, len(results))
-    
 
-    def help_greet(self):
+
+    def do_summarise(self, idx):    
+
+
+        try:
+            idx = int(idx)
+
+            if not self.check_idx(idx):
+                return
+
+
+            window_start = rostime_to_python(self.routine_pairs[idx][0].time)
+            window_end = rostime_to_python(self.routine_pairs[idx][1].time)
+
+            results = task_query.query_tasks(self.msg_store, 
+                        event=range(TaskEvent.TASK_STARTED, TaskEvent.ROUTINE_STARTED), 
+                        start_date=window_start,
+                        end_date=window_end,
+                        )
+            task_query.aggregate(results)
+
+
+        except ValueError, e:
+            print 'provided argument was not an int: %s' % idx
+
+    def do_executions(self, line):    
+        try:
+
+            tokens = line.split(' ')
+            idx = tokens[0]
+
+            if len(tokens) > 1 and len(tokens[1]) > 0:
+                action = tokens[1]
+                print 'only showing action: %s' % action
+            else:
+                action = None
+
+
+            idx = int(idx)
+
+            if not self.check_idx(idx):
+                return
+
+            print 'executions in routine %s' % idx
+            window_start = rostime_to_python(self.routine_pairs[idx][0].time)
+            window_end = rostime_to_python(self.routine_pairs[idx][1].time)
+
+            results = task_query.query_tasks(self.msg_store, 
+                        event=range(TaskEvent.TASK_STARTED, TaskEvent.ROUTINE_STARTED), 
+                        action=action,
+                        start_date=window_start,
+                        end_date=window_end,
+                        )
+            task_query.executions(results)
+
+
+        except ValueError, e:
+            print 'provided argument was not an int: %s' % idx
+            print e    
+
+    def help_print(self):
         print '\n'.join([ 'print', 'Print the available routines'])
 
     def help_merge(self):
-        print '\n'.join([ 'merge [idx]', 'Merge the routine at idx into the routine after it'])
+        print '\n'.join([ 'merge [idx | all]', 'Merge the routine at idx (int) into the routine after it. If idx is the string all, then merge all routines together.'])
+
+    def help_executions(self):
+        print '\n'.join([ 'merge [idx]', 'Show all task executions in routine at idx (int).'])
+
 
 
     def do_EOF(self, line):
@@ -65,13 +140,19 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Analyses the task execution behaviour in a routine window. Assumes all task ids are unique in this window.')
     parser.add_argument('start', metavar='S', type=task_query.mkdatetime, nargs='?', 
-                   help='start datetime of query, defaults to start of last routine. Formatted "d/m/y H:M" e.g. "06/07/14 06:38"')
+                   help='Start datetime of window for routines. Formatted "d/m/y H:M" e.g. "06/07/14 06:38"')
     
     parser.add_argument('end', metavar='E', type=task_query.mkdatetime, nargs='?', 
-                   help='end datetime of query, defaults to end of last routine or now for an unclosed routine. Formatted "d/m/y H:M" e.g. "06/07/14 06:38"')
+                   help='End datetime of window for routines. Formatted "d/m/y H:M" e.g. "06/07/14 06:38"')
 
     parser.add_argument('-t', '--tasks', type=int, default=1, nargs='?',
                     help='Number of tasks required for a routine to be considered')
+
+    parser.add_argument('-o', '--open', type=task_query.mktime, nargs='?', 
+                    help='Daily start time of the routine. Formatted "H:M" e.g. "06:38"')
+
+    parser.add_argument('-c', '--close', type=task_query.mktime, nargs='?', 
+                    help='Daily end time of the routine. Formatted "H:M" e.g. "17:45"')
 
     
     args = parser.parse_args()
@@ -85,10 +166,10 @@ if __name__ == '__main__':
         # make sure we start with a ROUTINE_STARTED
         while results[0][0].event == TaskEvent.ROUTINE_STOPPED:
             del results[0]
+            print 'pruned stop start'
 
         routines = []
         start = None
-        end = None
        
         # make sure we just have the pairs which are start/end
         for e in results:
@@ -115,7 +196,7 @@ if __name__ == '__main__':
                 filtered_routines.append(routines[i])
                 # print 'routine %s: %s to %s, duration: %s, tasks: %s' % (len(filtered_routines)-1, start, end, end-start, len(results))
 
-        RoutineAnalyser(filtered_routines).cmdloop()
+        RoutineAnalyser(msg_store, filtered_routines).cmdloop()
 
 
         # if args.start is None:
--- task_executor/src/task_executor/task_query.py
+++ task_executor/src/task_executor/task_query.py
@@ -6,7 +6,7 @@ import mongodb_store_msgs.srv as dc_srv
 import mongodb_store.util as dc_util
 from mongodb_store.message_store import MessageStoreProxy
 from strands_executive_msgs.msg import Task, TaskEvent
-from datetime import datetime
+from datetime import datetime, timedelta
 from task_executor import task_routine
 
 def remove_duplicates(results):
@@ -104,24 +104,96 @@ def print_event(task_event):
     """ Prints a single event """
     print format_event(task_event)
 
-def summarise(results):
+
+
+def event_time(task_event):
+    return datetime.utcfromtimestamp(task_event.time.to_sec()).strftime('%d/%m/%y %H:%M:%S')
+
+def mktime(time_string):
+    return datetime.strptime(time_string, '%H:%M').time()
+
+
+def mkdatetime(date_string):
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M')
+
+
+def start_of_new_group(event, open_group):
+    if event.event == TaskEvent.TASK_STARTED:
+        return True
+    elif len(open_group) == 0:
+        return False
+    # else:
+    #     return event.task.task_id != open_group[-1].task.task_id
+
+def group(results):
+    """ 
+    Groups events into individual executions
+    """
+
+    if len(results) == 0:
+        return []
+
+    groups = []
+    open_group = [results[0][0]]
+
+    for event, meta in results[1:]:
+        # start of an event group
+        if start_of_new_group(event, open_group):
+            groups.append(open_group)
+            open_group = []
+        
+        open_group.append(event)
+
+    # any remaining bits
+    if len(open_group) > 0:
+        groups.append(open_group)
+
+    return groups
+
+
+def aggregate(results):
+    if len(results) == 0:
+        print 'No task events match the query'
+        return 
+
+    event_groups = group(results)
+
+    by_action = {}
+
+    for event_group in event_groups:
+        action = event_group[0].task.action
+        if not action in by_action:
+            # [success, failed]
+            by_action[action] = [0, 0]
+
+        if any(te for te in event_group if te.event == TaskEvent.TASK_SUCCEEDED):
+            by_action[action][0] += 1
+        else:
+            by_action[action][1] += 1
+
+    for action, stats in by_action.iteritems():
+        print '%s: %s/%s' % (action, stats[0], sum(stats))
+
+
+
+
+
+def executions(results):
 
     if len(results) == 0:
         print 'No task events match the query'
         return 
+
+    groups = group(results)
         
     output = []
-    tid = 0
-
-    for task_event, meta in results:
-        if task_event.task.action == '':
-            task_event.task.action = 'no action'
-
-        if task_event.event > TaskEvent.DEMANDED:
-            if task_event.task.task_id != tid:
-                tid = task_event.task.task_id
-                output.append('\n')
-                       
+ 
+    for exec_group in groups:
+        output.append('\n')
+        for task_event in exec_group:
+            if task_event.task.action == '':
+                task_event.task.action = 'no action'            
+                                   
             output.append(['task %s' % task_event.task.task_id, task_event.task.action, task_event.task.start_node_id, task_event_string(task_event.event), datetime.utcfromtimestamp(task_event.time.to_sec()).strftime('%d/%m/%y %H:%M:%S')])
 
     # http://stackoverflow.com/a/9989441/135585
@@ -132,9 +204,118 @@ def summarise(results):
         else:
             print row
 
-def event_time(task_event):
-    return datetime.utcfromtimestamp(task_event.time.to_sec()).strftime('%d/%m/%y %H:%M:%S')
 
-def mkdatetime(date_string):
-    return datetime.strptime(date_string, '%d/%m/%y %H:%M')
+# def aggregate(results):
+
+#     duration = timedelta()
+#     charge_wait_duration = timedelta()
+
+#     started_task_event = TaskEvent()
+#     count = 0
+#     dubious = []
+#     charge_wait_count = 0
+#     unstarted_count = 0
+#     start_count = 0
+#     day_durations = {}
+
+#     for i in range(0, len(results)):
+
+#         if i > 0:
+#             previous = results[i-1][0]
+#         else:
+#             previous = None
+
+#         task_event = results[i][0]
+        
+#         if i < len(results) - 1:
+#             next = results[i+1][0]
+#         else:
+#             next = None
+
+#         start = False
+#         end = False
+
+#         if task_event.event == TaskEvent.TASK_STARTED or task_event.task.task_id != previous.task.task_id:
+#             start = True
+#         elif (task_event.event == TaskEvent.TASK_FINISHED or task_event.task.task_id != next.task.task_id or (task_event.task.action == '' and task_event.event == TaskEvent.NAVIGATION_SUCCEEDED)) and started_task_event.task.task_id != 0:
+#             end = True
+
+#         if start:
+#             started_task_event = task_event
+#             start_count += 1
+#         elif end:
+
+#             # if we're closing the previous event
+#             if task_event.task.task_id == started_task_event.task.task_id:
+        
+#                 end_time = datetime.utcfromtimestamp(task_event.time.to_sec())
+#                 task_duration = end_time - datetime.utcfromtimestamp(started_task_event.time.to_sec())
+
+#                 if task_event.task.action == 'wait_action' and task_event.task.start_node_id == 'ChargingPoint':
+#                     charge_wait_duration += task_duration
+#                     charge_wait_count += 1
+#                 else:
+                    
+#                     if task_duration > timedelta(hours=3):
+#                         dubious.append((started_task_event, task_event))
+#                         # task_query.print_event(started_task_event)
+#                         # task_query.print_event(task_event)                            
+#                         # print '%s\n' % task_duration                  
+#                     else:
+
+#                         task_date = end_time.date()
+
+#                         if task_date in day_durations:
+#                             day_durations[task_date].append(task_duration)
+#                         else:
+#                             day_durations[task_date] = [task_duration]
+
+#                         duration += task_duration
+#                         count += 1
+
+#                 # make sure we don't look for another end to this task
+#                 started_task_event = TaskEvent()
+
+#             else:
+#                 # print 'Finished an unstarted task: %s' % task_event
+#                 unstarted_count += 1
+#                 # task_query.print_event(started_task_event)
+#                 # task_query.print_event(task_event)
+#                 # print '\n'
+        
+#     start = results[0][0].time
+#     end = results[-1][0].time
+
+
+#     print 'Starts: %s' % start_count
+#     print 'Ends: %s (%s + %s + %s)' % (count + charge_wait_count  +  len(dubious), count, charge_wait_count, len(dubious))
+
+#     print 'Unstarted: %s' % unstarted_count
+#     print 'Dubious: %s' % len(dubious)
+
+#     print 'Tasks Completed: %s' % count        
+#     print 'Task Duration: %s' % duration
+
+#     print 'Charge Waits Finished: %s' % charge_wait_count        
+#     print 'Charge Waits Duration: %s' % charge_wait_duration
+
+#     print 'Tasks Start: %s' % datetime.utcfromtimestamp(start.to_sec())
+#     print 'Tasks End: %s' % datetime.utcfromtimestamp(end.to_sec())
+
+#     print 'Total activity span: %s ' % timedelta(seconds=(end - start).to_sec())
+
+#     days = day_durations.keys()
+#     days.sort()
+
+#     print 'Tasks and duration each day'
+#     total_possible = timedelta()
+#     for day in days:
+#         total_possible += timedelta(hours=9)
+#         durations = day_durations[day]
+#         total = timedelta()
+#         for d in durations:
+#             total += d
+#         print '%s:\t%s\t%s' % (day, len(durations), total)
+
+#     print 'Autonomy percentage: %.2f' % ((duration.total_seconds()/total_possible.total_seconds()) * 100)
 
