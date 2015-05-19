5ed46274825f9ac62ec4bb2d2165e13fb8b01b81
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-05-19 15:59:34 +0200

    Fixed formatting

--- task_executor/scripts/db_queries/analyse_routine.py
+++ task_executor/scripts/db_queries/analyse_routine.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
- -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import rospy
 import mongodb_store_msgs.srv as dc_srv
@@ -131,6 +131,11 @@ class RoutineAnalyser(cmd.Cmd):
         print '\n'.join([ 'summarise [idx]', 'Summarise task executions in routine idx (int).'])
 
 
+    def do_EOF(self, line):
+        return True
+
+    def help_EOF(self):
+        print 'Exit (CTRL-D)'
 
 
 if __name__ == '__main__':
