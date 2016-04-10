9942724cecb769390aeab0557469a1520f548e8c
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2016-04-10 13:11:46 +0100

    MDP executor debugged and short-term tested with normal and time-critical tasks.
    This is is still just responding to add_tasks and set_execution_status services, but should be reasonably robust.
    
    Ready for robot testing.

--- task_executor/src/task_executor/testing.py
+++ task_executor/src/task_executor/testing.py
@@ -43,9 +43,19 @@ class FakeActionServer(object):
                 # task_description[0] == self.master.node_id and 
                 if task_description[1] == self.action_string and task_description[2] == goal.some_goal_string and task_description[3] == goal.test_pose: 
                     print 'done task ', self.action_string, goal.some_goal_string
+
+                    # this this was a time value
+                    if goal.a_float > 1400000000:
+                        self.master.time_diffs.append(rospy.get_rostime().to_sec() - goal.a_float)
+
                     del self.master.task_descriptions[i]
+
+
                     self.server.set_succeeded()
                     return
+
+            
+
 
         self.server.set_aborted()
 
@@ -58,6 +68,7 @@ class TaskTester(object):
         self.action_sleep = action_sleep
         # self.node_id = ''
         self.task_descriptions = task_descriptions
+        self.time_diffs = []
         self.action_servers = [FakeActionServer(action_prefix + str(n), action_sleep, self, tester) for n in range(action_types)]
         self.state_lock = threading.Lock()
     
@@ -90,10 +101,9 @@ class TestEntry(object):
             rospy.sleep(1)
         return self.node_names
 
-    def run_test(self, task_descriptions_fn):   
+    def run_test(self, task_descriptions_fn, test_tasks = 20, time_critical_tasks = 0, time_diffs_fn = None):   
         waypoints = self.get_nodes()
         action_types = 5
-        test_tasks = 20
         action_sleep = rospy.Duration.from_sec(2)
         action_prefix = 'test_task_'
 
@@ -117,6 +127,19 @@ class TestEntry(object):
             task_start += action_sleep
         assert test_tasks == len(task_descriptions)
 
+        
+        time_critical_step = rospy.Duration(360) 
+        time_critical_start = rospy.get_rostime() + start_delay + rospy.Duration(120) 
+
+        for n in range(test_tasks, test_tasks + time_critical_tasks):            
+            string = 'Please run me at %s is' % time_critical_start.to_sec()
+            pose = Pose(Point(n, 1, 2), Quaternion(3, 4,  5, 6))        
+            task_descriptions += [[random.choice(waypoints), 
+                action_prefix + str(randrange(action_types)),
+                string,
+                pose, n, time_critical_start.to_sec(), time_critical_start, time_critical_start]]
+            time_critical_start += time_critical_step
+ 
         task_tester = TaskTester(action_types, action_prefix, task_descriptions, action_sleep, self)    
 
         # get task services
@@ -151,14 +174,23 @@ class TestEntry(object):
             # Start the task executor running
             set_execution_status(True)
 
-            max_wait_duration = start_delay
+            max_wait_duration = start_delay 
+            if time_critical_tasks > 0:
+                max_wait_duration += time_critical_start - rospy.get_rostime()
+
             max_travel = rospy.Duration(90)
             for n in range(test_tasks):
                 max_wait_duration += (action_sleep + max_travel)
 
             task_tester.wait_for_completion(max_wait_duration)
-            if task_descriptions_fn is not None:
-                task_descriptions_fn(task_tester.task_descriptions)
+
+            with task_tester.state_lock:
+
+                if task_descriptions_fn is not None:
+                    task_descriptions_fn(task_tester.task_descriptions)
+
+                if time_critical_tasks > 0 and time_diffs_fn is not None:
+                    time_diffs_fn(task_tester.time_diffs)                
 
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
