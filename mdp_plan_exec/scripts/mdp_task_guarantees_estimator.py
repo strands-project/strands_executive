4b738aa19c87fe51c6176b4553bbbceaf29f7cc2
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2016-04-20 20:29:14 +0100

    task guarantees service now needs initial waypoint, and outputs the values for prob, prog, and time

--- mdp_plan_exec/scripts/mdp_task_guarantees_estimator.py
+++ mdp_plan_exec/scripts/mdp_task_guarantees_estimator.py
@@ -7,7 +7,8 @@ from copy import deepcopy
 import rospy
 import threading
 from mdp_plan_exec.top_map_mdp import TopMapMdp
-from mdp_plan_exec.prism_java_talker import PrismJavaTalker
+from mdp_plan_exec.policy_mdp import PolicyMdp
+from mdp_plan_exec.partial_sat_prism_java_talker import PartialSatPrismJavaTalker
 
 from strands_executive_msgs.srv import GetGuaranteesForCoSafeTask, GetGuaranteesForCoSafeTaskResponse
 
@@ -18,6 +19,7 @@ class MdpTaskGuaranteesEstimator(object):
         
         self.top_map_mdp=TopMapMdp(top_map, explicit_doors=True)
         self.current_extended_mdp=None
+        self.policy_mdp=None
         self.directory = os.path.expanduser("~") + '/tmp/prism/guarantees_estimator/'
         self.service_lock = threading.Lock()
         try:
@@ -25,8 +27,7 @@ class MdpTaskGuaranteesEstimator(object):
         except OSError as ex:
             print 'error creating PRISM directory:',  ex
         self.file_name=top_map+".mdp"
-        self.prism_estimator=PrismJavaTalker(8087,self.directory, self.file_name)
-        self.last_epoch=-1
+        self.prism_estimator=PartialSatPrismJavaTalker(8087,self.directory, self.file_name)
         self.get_guarantees_service = rospy.Service('/mdp_plan_exec/get_guarantees_for_co_safe_task',
                                                               GetGuaranteesForCoSafeTask,
                                                               self.get_guarantees_cb)
@@ -37,20 +38,28 @@ class MdpTaskGuaranteesEstimator(object):
 
     def get_guarantees_cb(self,req):
         with self.service_lock:
+            response=GetGuaranteesForCoSafeTaskResponse()
             self.current_extended_mdp=deepcopy(self.top_map_mdp)
+            self.current_extended_mdp.set_initial_state_from_waypoint(req.initial_waypoint)
             self.current_extended_mdp.add_extra_domain(req.spec.vars, req.spec.actions)
-            if req.epoch != self.last_epoch:
-                self.last_epoch=req.epoch
-            self.current_extended_mdp.set_mdp_action_durations(self.directory+self.file_name, req.epoch, set_initial_state=False) #TODO we need this here to update the MDP model file, but we shouldn't need to get new action durations to do it when the epoch didnt change.
+            self.current_extended_mdp.set_mdp_action_durations(self.directory+self.file_name, req.epoch, set_initial_state=True) #add epoch - guarantees dict?
             specification=self.generate_prism_specification(req.spec.ltl_task)
             rospy.loginfo("The specification is " + specification)
-            state_vector=self.prism_estimator.get_state_vector(specification, is_partial=True)
-            (waypoint_names, waypoint_probs, waypoint_progs, waypoint_times)=self.current_extended_mdp.map_state_vectors_to_waypoint_expectations(self.directory+'original.sta', state_vector)
-            return GetGuaranteesForCoSafeTaskResponse(initial_waypoints=waypoint_names,
-                                                      probabilities=waypoint_probs,
-                                                      prog_rewards=waypoint_progs,
-                                                      expected_times=map(rospy.Duration, waypoint_times))
-        
+            prism_call_success=self.prism_estimator.call_prism(specification)
+            if prism_call_success:
+                self.policy_mdp=PolicyMdp(self.current_extended_mdp,
+                                          self.directory + '/prod.aut',
+                                          self.directory + '/prod.sta',
+                                          self.directory + '/prod.lab',
+                                          self.directory+'adv.tra',
+                                          self.directory+'guarantees1.vect',
+                                          self.directory+'guarantees2.vect',
+                                          self.directory+'guarantees3.vect')
+                (response.probability, response.prog_reward, response.expected_time)=self.policy_mdp.get_guarantees_at_initial_state()
+                return response
+            else:
+                rospy.logwarn("Error calling PRISM, guarantees extimator service returning empty response")
+                return response
 
     def main(self):
        # Wait for control-c
