50502a3f82ecbfdde2e10c8ba78e3e8b712ae0b7
Bruno Lacerda <bruno@robots.ox.ac.uk>
2018-06-28 12:18:44 +0100

    stop redundant predictions

--- mdp_plan_exec/src/mdp_plan_exec/top_map_mdp.py
+++ mdp_plan_exec/src/mdp_plan_exec/top_map_mdp.py
@@ -60,7 +60,9 @@ class TopMapMdp(Mdp):
                             'action_descriptions':deepcopy(self.action_descriptions),
                             'transitions':deepcopy(self.transitions)}
         
-              
+        self.last_prediction_epoch = rospy.Time()
+        self.get_new_preds_threshold = rospy.Duration(60*5)
+        
         rospy.loginfo("Topological MDP initialised")
 
 
@@ -380,9 +382,11 @@ class TopMapMdp(Mdp):
     def add_predictions(self, file_name, epoch=None, set_initial_state=True):
         if epoch is None:
             epoch=rospy.Time.now()
-        self.add_nav_predictions(epoch)
-        if self.explicit_doors:
-            self.add_door_predictions(epoch)
+        if self.last_prediction_epoch - epoch >  self.get_new_preds_threshold or epoch - self.last_prediction_epoch > self.get_new_preds_threshold:
+            self.add_nav_predictions(epoch)
+            self.last_prediction_epoch = epoch
+            if self.explicit_doors:
+                self.add_door_predictions(epoch)
         self.write_prism_model(file_name, set_initial_state)
     
 
